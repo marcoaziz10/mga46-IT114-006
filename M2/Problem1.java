50b32fb7c1bfca62e5a7b0ed119296fd3bdaa861
Marco Aziz <gmmh2006@gmail.com>
2025-02-17 15:46:42 -0500

    Update Problem1.java

--- M2/Problem1.java
+++ M2/Problem1.java
@@ -1,4 +1,7 @@
 package M2;
+// UCID: mga46
+// Date: February 17, 2025
+// Summary: Solution for #1 - Print odd values from the arrays in a single line
 
 public class Problem1 extends BaseClass {
     private static int[] array1 = {0,1,2,3,4,5,6,7,8,9};   
@@ -15,7 +18,14 @@ public class Problem1 extends BaseClass {
         // Step 3: Add code to solve the problem (add/commit as needed)
         System.out.print("Output Array: ");
         // Start Solution Edits
-        
+         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[i]);
+            }
+        }
 
         // End Solution Edits
         System.out.println("");
