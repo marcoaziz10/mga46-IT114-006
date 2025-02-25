1533b0c0218d81745f5d3f1c88c281811b4df036
Marco Aziz <gmmh2006@gmail.com>
2025-02-24 23:59:57 -0500

    Fixed loop control in SlashCommandHandler

--- M3/MadLibsGenerator.java
+++ M3/MadLibsGenerator.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 /*
 Challenge 3: Mad Libs Generator (Randomized Stories)
 -----------------------------------------------------
